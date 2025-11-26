3b8485cd0ede752afca55bd75531c144d77e3105
Rodolfo <fororus12@gmail.com>
2025-11-26 16:15:14 -0400

    Solved resolution problems & unnecesary props deleted

--- src/app/components/main/vehicles/VehicleGallery.tsx
+++ src/app/components/main/vehicles/VehicleGallery.tsx
@@ -45,15 +45,8 @@ export const VehicleGallery = ({
       setSelectedThumbnailIndex(index);
   };
 
-  // Determina la URL de la imagen principal basada en el color seleccionado.
-  // const mainColorImageUrl = imagesByColor[selectedColor] ?? imagesByColor[colors[0].name];
-  // const thumbnailOverrideUrl = thumbnails[selectedThumbnailIndex]?.url;
-  
-  // Si la primera miniatura (índice 0) está seleccionada, se usa la imagen del color principal,
-  // si no, se usa la imagen de la miniatura seleccionada.
-  // const currentImageUrl = selectedThumbnailIndex === 0 ? mainColorImageUrl : thumbnailOverrideUrl;  
   const currentImageUrl = selectedThumbnails[selectedThumbnailIndex]?.url ?? "";
-
+  
   return (
     <div className="lg:col-span-3">
       <div className="flex flex-col md:flex-row gap-4">
@@ -66,14 +59,14 @@ export const VehicleGallery = ({
 
         <div className="flex-1 order-1 md:order-2">
           <div
-            className="aspect-[4/3] w-full bg-center bg-no-repeat bg-cover flex flex-col justify-end overflow-hidden rounded-xl cursor-pointer"
+            className="aspect-4/3 w-full bg-center bg-no-repeat bg-cover flex flex-col justify-end overflow-hidden rounded-xl cursor-pointer"
             data-alt={`Vista frontal del Toyota Corolla XSE color ${selectedColor}`}
             style={{ backgroundImage: `url("${ currentImageUrl }")`}}
             >
-            <div className="p-4 bg-gradient-to-t from-black/50 to-transparent">
+            <div className="p-4 bg-linear-to-t from-black/50 to-transparent">
               <span
                 className="inline-flex items-center gap-2 text-white bg-black/30 backdrop-blur-sm px-3 py-1 rounded-full text-sm">
-                <span className="material-symbols-outlined !text-base">zoom_in</span>
+                <span className="material-symbols-outlined text-base!">zoom_in</span>
                 Haz clic para ampliar
               </span>
             </div>
