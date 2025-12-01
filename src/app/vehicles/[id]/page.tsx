3025bf2e913df151228ca27bf9392ff21714fc2e
Rodolfo <fororus12@gmail.com>
2025-12-01 00:11:24 -0400

    Metadata for vehicles and vehicle

--- src/app/vehicles/[id]/page.tsx
+++ src/app/vehicles/[id]/page.tsx
@@ -1,12 +1,33 @@
-
-
 import { getVehicle } from '@/src/api/Vehicles';
 import { PriceAndActions } from '../../components/main/vehicles/PriceAndActions';
 import { VehicleGallery } from '../../components/main/vehicles/VehicleGallery';
+import { Metadata } from 'next';
 
 interface Props{
   params: { id: string };
 }
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { id } = await params;
+    const vehicle = await getVehicle( id );
+
+  return {
+    title: `FalconMotors | ${vehicle.brand} ${vehicle.vehicleModel} ${vehicle.year}`,
+    description: `Descubre el ${vehicle.brand} ${vehicle.vehicleModel} ${vehicle.year}. Precio: ${vehicle.price}. Disponible en FalconMotors con garantía y opciones de financiamiento.`,
+    keywords: [
+      vehicle.brand,
+      vehicle.vehicleModel,
+      `${vehicle.year}`,
+      "autos nuevos",
+      "autos usados",
+      "vehículos FalconMotors",
+      "compra de autos",
+      "financiamiento de autos",
+    ],
+  };
+}
+
+
 
 export default async function DetailedVehiclePage({ params }: Props) {
 
@@ -15,7 +36,7 @@ export default async function DetailedVehiclePage({ params }: Props) {
   
   return (
     <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-16">
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 lg:gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 lg:gap-12 lg:items-start">
 
         <VehicleGallery 
           colors={ vehicle.colors }
--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -6,6 +6,13 @@ import { SearchBar } from "../components/main/vehicles/SearchBar";
 export const metadata: Metadata = {
   title: 'Inventario de Autos | Encuentra tu Vehículo | FalconMotors',
   description: 'Explora nuestro amplio y actualizado inventario de vehículos nuevos y usados. Los mejores modelos, precios y opciones de financiamiento en Punto Fijo.',
+  keywords: [
+    "autos nuevos",
+    "autos usados",
+    "vehículos FalconMotors",
+    "compra de autos",
+    "financiamiento de autos",
+  ]
 };
 
 export default function VehiclePage() {
@@ -14,13 +21,11 @@ export default function VehiclePage() {
     <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-16">
       <div className="flex flex-col lg:flex-row lg:gap-8">
         <Filters />
-
         <div className="w-full lg:w-3/4 xl:w-4/5">
           <SearchBar />
+
           <VehicleList />
-
         </div>
-
       </div>
     </main>
   );
