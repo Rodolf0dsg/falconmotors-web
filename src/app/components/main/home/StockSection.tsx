a75dce1e3534262b49c5a37cb6e640599912f383
Rodolfo <fororus12@gmail.com>
2025-12-15 20:43:17 -0400

    solved resolution problem on link

--- src/app/components/main/home/StockSection.tsx
+++ src/app/components/main/home/StockSection.tsx
@@ -57,7 +57,7 @@ export const StockSection = () => {
           <Link
             href={'/vehicles'}
             className="p-2 rounded-md bg-red-600 text-white text-xs md:text-sm font-semibold 
-                       border border-gray-300 hover:bg-red-700 transition-colors h-10 cursor-pointer"
+                       border border-gray-300 hover:bg-red-700 transition-colors h-10 w-20 cursor-pointer"
           >
             Ver más
           </Link>
